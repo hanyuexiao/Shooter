d2cade5f87d85be943a19d58838a4c62bb1191ec
hanyuexiao <91308791+hanyuexiao@users.noreply.github.com>
2025-10-17 00:56:34 +0800

    Delete Source/Inventory directory

--- Source/Inventory/Private/Interaction/Inv_HighLightable.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interaction/Inv_HighLightable.h"
-
-// Add default functionality here for any IInv_HighLightable functions that are not pure virtual.
--- Source/Inventory/Private/Widgets/Composite/Inv_Leaf.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Widgets/Composite/Inv_Leaf.h"
-
-void UInv_Leaf::ApplyFunction(FuncType Function)
-{
-	Function(this);
-}
